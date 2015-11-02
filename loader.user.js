0f51f54f86570b794d48d1f81bfe8fc74c770fee
xmdy <xmdy.pro@gmail.com>
2015-11-02 19:35:01 +0300

    change loader links

--- loader.user.js
+++ loader.user.js
@@ -12,26 +12,26 @@ var sha = "efde0488cc2cc176db48dd23b28a20b90314352b";
 
 function getLatestCommit() {
 	window.jQuery.ajax({
-		url: "https://api.github.com/repos/apostolique/Agar.io-bot/git/refs/heads/master",
+		url: "https://api.github.com/repos/xmdy/Agar.io-bot/git/refs/heads/master",
 		cache: false,
 		dataType: "jsonp"
 	}).done(function(data) {
 		console.dir(data["data"])
 		console.log("hmm: " + data["data"]["object"]["sha"]);
 		sha = data["data"]["object"]["sha"];;
-		window.jQuery.get('https://raw.githubusercontent.com/Apostolique/Agar.io-bot/master/launcher.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
+		window.jQuery.get('https://raw.githubusercontent.com/xmdy/Agar.io-bot/master/launcher.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
 			var latestVersion = data.replace(/(\r\n|\n|\r)/gm, "");
 			latestVersion = latestVersion.substring(latestVersion.indexOf("// @version") + 11, latestVersion.indexOf("// @grant"));
 			latestVersion = parseFloat(latestVersion + 0.0000);
-			var script1 = "https://cdn.rawgit.com/Apostolique/Agar.io-bot/" + sha + "/launcher.user.js";
+			var script1 = "https://cdn.rawgit.com/xmdy/Agar.io-bot/" + sha + "/launcher.user.js";
 			console.log("Script: " + script1);
 			window.jQuery("body").append('<script type="text/javascript" src="' + script1 + '"></script>');
 		});
-		window.jQuery.get('https://raw.githubusercontent.com/Apostolique/Agar.io-bot/master/bot.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
+		window.jQuery.get('https://raw.githubusercontent.com/xmdy/Agar.io-bot/master/bot.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
 			var latestVersion = data.replace(/(\r\n|\n|\r)/gm, "");
 			latestVersion = latestVersion.substring(latestVersion.indexOf("// @version") + 11, latestVersion.indexOf("// @grant"));
 			latestVersion = parseFloat(latestVersion + 0.0000);
-			var script2 = "https://cdn.rawgit.com/Apostolique/Agar.io-bot/" + sha + "/bot.user.js";
+			var script2 = "https://cdn.rawgit.com/xmdy/Agar.io-bot/" + sha + "/bot.user.js";
 			console.log("Script: " + script2);
 			window.jQuery("body").append('<script type="text/javascript" src="' + script2 + '"></script>');
 		});
@@ -47,13 +47,13 @@ function getLatestCommit() {
 			});
 			window.jQuery("#" + prefix + "Dialog").show();
 		}
-		window.jQuery.get('https://raw.githubusercontent.com/Apostolique/Agar.io-bot/master/loader.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
+		window.jQuery.get('https://raw.githubusercontent.com/xmdy/Agar.io-bot/master/loader.user.js?' + Math.floor((Math.random() * 1000000) + 1), function(data) {
 			var latestVersion = data.replace(/(\r\n|\n|\r)/gm, "");
 			latestVersion = latestVersion.substring(latestVersion.indexOf("// @version") + 11, latestVersion.indexOf("// @grant"));
 			latestVersion = parseFloat(latestVersion + 0.0000);
 			var myVersion = parseFloat(aposLoaderVersion + 0.0000);
 			if (latestVersion > myVersion) {
-				update("aposLoader", "loader.user.js", "https://github.com/Apostolique/Agar.io-bot/blob/master/loader.user.js/");
+				update("aposLoader", "loader.user.js", "https://github.com/xmdy/Agar.io-bot/blob/master/loader.user.js/");
 			}
 			console.log('Current loader.user.js Version: ' + myVersion + " on Github: " + latestVersion);
 		});
